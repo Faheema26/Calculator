093727ccb446d00b953464828e23fd351927c9ea
Faheema26 <faheemadamam@gmail.com>
2023-03-01 12:01:29 -0500

    hi

---
+++ Day-1.py
@@ -0,0 +1,26 @@
+#1 print("Hello World")
+
+#2x="Shannu"
+#print(x)
+
+#3fruits=['apple', 'banana']
+#fruits.append('mango')
+#print(fruits)
+
+#4dishes=('biryani', 'noodles')
+#temp=list(dishes)
+#temp.append('dal')
+#dishes=tuple(temp)
+#print(dishes)
+
+#5dict={"name": "faheema",
+#"class": 1,
+#"grade":"A",}
+#print(dict)
+
+#6x={}
+#x["name"]="faheema", "shannu", "dakshayini","shivani", "raziya"
+#x["class"]=1,2,3,4,5
+#x["grade"]="A", "B", "A", "A", "B"
+#print(x)
+
