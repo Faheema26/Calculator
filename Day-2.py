66894f2369680b5b4d493fb19b295c9ec9870eb9
Faheema26 <faheemadamam@gmail.com>
2023-03-03 09:42:07 -0500

    This is my push

---
+++ Day-2.py
@@ -0,0 +1,40 @@
+# calculator using if else program
+#print('calculator')
+#print('1.Add')
+#print('2.Sub')
+#print('3.Mul')
+#print('4.div')
+#a = int(input('Enter the value:'))
+#b = int(input('Enter the value:'))
+#x = int(input("Enter your operation from 1 to 4:"))
+#if x==1:
+    #c = a+b
+    #print('add=',c)
+#elif x==2:
+    #c=a-b
+    #print('sub=',c)
+#elif x==3:
+   # c=a*b
+    #print('Mul=',c)
+#elif x==4:
+    #c=a/b
+    #print('Div=',c)
+#else:
+    #print('invalid choice')
+
+#example 1 for loop
+#fruits=['apple','banana','mango','orange','pineapple','strawberry']
+#for i in fruits:
+    #print(i)
+
+#example 2 for loop
+#n=int(input("enter a range:"))
+#for i in range(n):
+    #print(i**2)
+    #print((pow(i,2)))
+
+#example 3 while loop
+x=1
+while x<=11:
+    print('faheema')
+    x=x+1
